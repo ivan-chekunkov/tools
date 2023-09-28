c2da064a90c8600e3583620a99ae03ea84b9f483
ivan-chekunkov <chekunkov.ivan@yandex.ru>
2023-09-28 14:28:31 +0300

    reg_key_correct

--- time_change.py
+++ time_change.py
@@ -1,3 +1,6 @@
+import datetime
+import json
+import sys
 import winreg
 
 
@@ -30,6 +33,28 @@ class RegEdit:
         print("enable_ubdate_time")
         winreg.SetValueEx(key, "Type", None, winreg.REG_SZ, "NTP")
         winreg.CloseKey(key)
+
+
+def get_date_today() -> tuple[int, int, int]:
+    today = datetime.datetime.now()
+    return today.year, today.month, today.day
+
+
+def get_date_from_file() -> tuple:
+    try:
+        with open("date.json", "r", encoding="utf-8") as file:
+            data = json.load(file)
+    except Exception as error:
+        print(error)
+        sys.exit()
+    year, month, day = get_date_today()
+    try:
+        year = int(data.get("year", year))
+        month = int(data.get("month", month))
+        day = int(data.get("day", day))
+    except Exception as error:
+        print(error)
+    return year, month, day
 
 
 if __name__ == "__main__":
