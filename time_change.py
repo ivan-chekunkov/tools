f74ddcff036f9616c60b0f8f8d37638a71e1e0a2
Ivan Chekunkov <chekunkov.ivan@yandex.ru>
2023-10-01 00:07:18 +0300

    correct_timechange

--- time_change.py
+++ time_change.py
@@ -112,5 +112,16 @@ def get_date_from_arguments(arguments: list[str]):
     return year, month, day
 
 
+def check_arguments():
+    arguments = sys.argv[1:]
+    if len(arguments) == 0:
+        return get_date_from_file()
+    if arguments[0].lower() in ("help" or "h"):
+        help_argument()
+    if arguments[0].lower() in ("clear" or "c"):
+        clear_date()
+    return get_date_from_arguments(arguments)
+
+
 if __name__ == "__main__":
     pass
