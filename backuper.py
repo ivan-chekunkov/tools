e690e23ffb7f69f20c0e493ea41f6e65f6bc7485
Ivan Chekunkov <chekunkov.ivan@yandex.ru>
2023-10-03 23:05:17 +0300

    add_backuper

---
+++ backuper.py
@@ -0,0 +1,23 @@
+import logging
+
+
+def get_logger(name: str, level: int) -> logging.Logger:
+    logger = logging.getLogger(name)
+    file_log = logging.FileHandler(
+        "{}.log".format(name.capitalize()), encoding="UTF-8"
+    )
+    console_out = logging.StreamHandler()
+    formatter = logging.Formatter(
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    )
+    logger.setLevel(level)
+    console_out.setFormatter(formatter)
+    logger.addHandler(console_out)
+    file_log.setFormatter(formatter)
+    logger.addHandler(file_log)
+    return logger
+
+
+main_log = get_logger("main_logger", logging.INFO)
+success_log = get_logger("success_logger", logging.INFO)
+error_log = get_logger("error_logger", logging.ERROR)
